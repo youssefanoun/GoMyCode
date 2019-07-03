e34c80ba19b5ce57cb1c8c42b73b8122f48c84eb
youssefanoun <47716658+youssefanoun@users.noreply.github.com>
2019-07-03 15:27:52 +0100

    Add files via upload

---
+++ check 5/main.js
@@ -0,0 +1,47 @@
+$("#main").mouseenter(function(){
+    $("#child").slideToggle("slow");
+});
+
+$("#main").mouseleave(function(){
+    $("#child").slideToggle("slow");
+});
+
+$("#main1").mouseenter(function(){
+    $("#child1").slideToggle("slow");
+});
+
+$("#main1").mouseleave(function(){
+    $("#child1").slideToggle("slow");
+});
+
+$("#main2").mouseenter(function(){
+    $("#child2").slideToggle("slow");
+});
+
+$("#main2").mouseleave(function(){
+    $("#child2").slideToggle("slow");
+});
+
+$("#main3").mouseenter(function(){
+    $("#child3").slideToggle("slow");
+});
+
+$("#main3").mouseleave(function(){
+    $("#child3").slideToggle("slow");
+});
+
+$("#main4").mouseenter(function(){
+    $("#child4").slideToggle("slow");
+});
+
+$("#main4").mouseleave(function(){
+    $("#child4").slideToggle("slow");
+});
+
+$("#main5").mouseenter(function(){
+    $("#child5").slideToggle("slow");
+});
+
+$("#main5").mouseleave(function(){
+    $("#child5").slideToggle("slow");
+});
